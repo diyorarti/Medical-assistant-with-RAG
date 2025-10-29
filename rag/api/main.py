a801416eb6c222fc4c32725947e039bce9799730
diyorarti <diyorarti@gmail.com>
2025-10-29 06:32:04 +0100

    api folder structura and health endpoint

--- rag/api/main.py
+++ rag/api/main.py
@@ -1,24 +1,20 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import health
+
+from rag.api.routers import health
 
 def create_app() -> FastAPI:
-    app = FastAPI(title="Medical RAG API", version="1.0.0")
+    app = FastAPI(title="Med assistant by RAG API", version="1.0")
 
     app.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
-        allow_headers=["*"]
+        allow_headers=["*"],
     )
-
     app.include_router(health.router)
 
-    @app.get("/")
-    def root():
-        return {"message":"RAG API is up. See /docs"}
-    
     return app
 
 app = create_app()
